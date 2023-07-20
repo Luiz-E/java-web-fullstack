73a09496d517470d839de23c172a5375cb02d0bd
Luiz_E <luizedumartins21@gmail.com>
2023-07-20 20:44:45 -0300

    Módulo 9: Introdução a Orientação a Objetos - Aula 9.7: Sobre o construtor do objeto

--- src/main/java/org/example/classes/Aluno.java
+++ src/main/java/org/example/classes/Aluno.java
@@ -12,4 +12,14 @@ public class Aluno {
     String nomeEscola;
     String serieMatriculado;
 
+    public Aluno() {}
+    public Aluno(String nomePadrao) {
+        this.nome = nomePadrao;
+    }
+
+    public Aluno (String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
 }
--- src/main/java/org/example/executavel/Main.java
+++ src/main/java/org/example/executavel/Main.java
@@ -1,7 +1,13 @@
 package org.example.executavel;
 
+import org.example.classes.Aluno;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Aluno aluno1 = new Aluno();
+        Aluno aluno2 = new Aluno();
+        Aluno aluno3 = new Aluno();
+        Aluno aluno4 = new Aluno("Maria");
+        Aluno aluno5 = new Aluno("José", 50);
     }
 }
