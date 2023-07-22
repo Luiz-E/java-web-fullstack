310917ff8e59b067987119c4b4a76f4b1d7ddc01
Luiz_E <luizedumartins21@gmail.com>
2023-07-21 23:07:38 -0300

    Criação da Branch módulo 9; remoção dos arquivos presentes na branch main

--- src/main/java/org/example/executavel/Main.java
+++ src/main/java/org/example/executavel/Main.java
@@ -1,13 +1,7 @@
 package org.example.executavel;
 
-import org.example.classes.Aluno;
-
 public class Main {
     public static void main(String[] args) {
-        Aluno aluno1 = new Aluno();
-        Aluno aluno2 = new Aluno();
-        Aluno aluno3 = new Aluno();
-        Aluno aluno4 = new Aluno("Maria");
-        Aluno aluno5 = new Aluno("José", 50);
+
     }
 }
