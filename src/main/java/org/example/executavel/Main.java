f52193f4a9332feb8e8528191dee3ec2c94ca924
Luiz_E <luizedumartins21@gmail.com>
2023-07-21 23:06:36 -0300

    Módulo 9: Introdução a Orientação a Objetos - Aula 9.8: Declaração de tipos públicos e privados

--- src/main/java/org/example/executavel/Main.java
+++ src/main/java/org/example/executavel/Main.java
@@ -5,7 +5,12 @@ import org.example.classes.Aluno;
 public class Main {
     public static void main(String[] args) {
         Aluno aluno1 = new Aluno();
-        Aluno aluno2 = new Aluno();
+        aluno1.nome = "João";
+        System.out.println("Nome: " + aluno1.nome);
+
+        //aluno1.idade;
+
+        Aluno aluno2 = null;
         Aluno aluno3 = new Aluno();
         Aluno aluno4 = new Aluno("Maria");
         Aluno aluno5 = new Aluno("José", 50);
