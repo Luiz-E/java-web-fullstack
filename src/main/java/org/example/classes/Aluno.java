ca00d74daf4ed8e22fb41223b55d86021b0d5c44
Luiz_E <luizedumartins21@gmail.com>
2023-07-27 22:26:45 -0300

    Módulo 9: Introdução a Orientação a Objetos - Aula 9.17: criando atributos de disciplina do Aluno

--- src/main/java/org/example/classes/Aluno.java
+++ src/main/java/org/example/classes/Aluno.java
@@ -15,9 +15,13 @@ public class Aluno {
     private String serieMatriculado;
 
     private double nota1;
+    private String disciplina1;
     private double nota2;
+    private String disciplina2;
     private double nota3;
+    private String disciplina3;
     private double nota4;
+    private String disciplina4;
 
     public Aluno() {}
     public Aluno(String nomePadrao) {
@@ -166,12 +170,14 @@ public class Aluno {
                 ", dataMatricula='" + dataMatricula + '\'' +
                 ", nomeEscola='" + nomeEscola + '\'' +
                 ", serieMatriculado='" + serieMatriculado + '\'' +
-                ", nota1=" + nota1 + '\'' +
-                ", nota2=" + nota2 + '\'' +
-                ", nota3=" + nota3 + '\'' +
-                ", nota4=" + nota4 + '\'' +
-                ", media=" + getMediaNota() + '\'' +
-                ", aprovado=" + getAlunoAprovado2() + '\'' +
+                ", nota1=" + nota1 +
+                ", disciplina1='" + disciplina1 + '\'' +
+                ", nota2=" + nota2 +
+                ", disciplina2='" + disciplina2 + '\'' +
+                ", nota3=" + nota3 +
+                ", disciplina3='" + disciplina3 + '\'' +
+                ", nota4=" + nota4 +
+                ", disciplina4='" + disciplina4 + '\'' +
                 '}';
     }
 
@@ -187,4 +193,37 @@ public class Aluno {
     public int hashCode() {
         return Objects.hash(nome, numeroCpf);
     }
+
+    public String getDisciplina1() {
+        return disciplina1;
+    }
+
+    public void setDisciplina1(String disciplina1) {
+        this.disciplina1 = disciplina1;
+    }
+
+    public String getDisciplina2() {
+        return disciplina2;
+    }
+
+    public void setDisciplina2(String disciplina2) {
+        this.disciplina2 = disciplina2;
+    }
+
+    public String getDisciplina3() {
+        return disciplina3;
+    }
+
+    public void setDisciplina3(String disciplina3) {
+        this.disciplina3 = disciplina3;
+    }
+
+    public String getDisciplina4() {
+        return disciplina4;
+    }
+
+    public void setDisciplina4(String disciplina4) {
+        this.disciplina4 = disciplina4;
+    }
 }
+
--- src/main/java/org/example/executavel/Main.java
+++ src/main/java/org/example/executavel/Main.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        /*
+
         Aluno aluno = new Aluno();
         String nome = JOptionPane.showInputDialog("Qual o nome do aluno? ");
         String idade = JOptionPane.showInputDialog("Qual a idade?");
@@ -18,9 +18,14 @@ public class Main {
         String dataMatricula = JOptionPane.showInputDialog("Qual a data da matrícula?");
         String serie = JOptionPane.showInputDialog("Qual a serie?");
         String escola = JOptionPane.showInputDialog("Qual a escola?");
+
+        String disciplina1 = JOptionPane.showInputDialog("Qual a disciplina 1?");
         String nota1 = JOptionPane.showInputDialog("Qual a nota 1?");
+        String disciplina2 = JOptionPane.showInputDialog("Qual a disciplina 2?");
         String nota2 = JOptionPane.showInputDialog("Qual a nota 2?");
+        String disciplina3 = JOptionPane.showInputDialog("Qual a disciplina 3?");
         String nota3 = JOptionPane.showInputDialog("Qual a nota 3?");
+        String disciplina4 = JOptionPane.showInputDialog("Qual a disciplina 4?");
         String nota4 = JOptionPane.showInputDialog("Qual a nota 4?");
 
         aluno.setNome(nome);
@@ -37,21 +42,12 @@ public class Main {
         aluno.setNota1(Double.parseDouble(nota1));
         aluno.setNota2(Double.parseDouble(nota2));
         aluno.setNota3(Double.parseDouble(nota3));
-        aluno.setNota4(Double.parseDouble(nota4));*/
+        aluno.setNota4(Double.parseDouble(nota4));
 
-        // EQUALS & HASHCODE
+        aluno.setDisciplina1(disciplina1);
+        aluno.setDisciplina2(disciplina2);
+        aluno.setDisciplina3(disciplina3);
+        aluno.setDisciplina4(disciplina4);
 
-        Aluno aluno1 = new Aluno();
-        aluno1.setNome("Alex");
-        aluno1.setNumeroCpf("123");
-        Aluno aluno2 = new Aluno();
-        aluno2.setNome("Alex");
-        aluno2.setNumeroCpf("1234");
-
-        if (aluno1.equals(aluno2)) {
-            System.out.println("alunos são iguais");
-        } else {
-            System.out.println("alunos não são iguais");
-        }
     }
 }
