5ccab0e3f432a6dd9aae1a6507e930b36e2935b7
Luiz_E <luizedumartins21@gmail.com>
2024-01-28 15:28:24 -0300

    Módulo 13: Arrays[Vetores] e Matrizes - Aula 11: Introdução a matrizes ou arrays multidimensionais; Aula 12: Criando uma Matriz multidimensional

---
+++ src/main/java/org/example/executavel/Matriz.java
@@ -0,0 +1,16 @@
+package org.example.executavel;
+
+import java.util.Arrays;
+
+public class Matriz {
+    public static void main(String[] args) {
+        double[][] notas = new double[2][3];
+        notas[0][0] = 80;
+        notas[0][1] = 90;
+        notas[0][2] = 70;
+
+        notas[1][0] = 60;
+        notas[1][1] = 85;
+        notas[1][2] = 90;
+    }
+}
