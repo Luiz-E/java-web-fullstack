a2bf65fb96ef51058d47f533c20be80e37abd2cb
Luiz_E <luizedumartins21@gmail.com>
2024-01-28 15:44:51 -0300

    Módulo 13: Arrays[Vetores] e Matrizes - Aula 15: Conversão Array para Lista e Lista para Array

--- src/main/java/org/example/executavel/SplitArray.java
+++ src/main/java/org/example/executavel/SplitArray.java
@@ -1,6 +1,8 @@
 package org.example.executavel;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class SplitArray {
     public static void main(String[] args) {
@@ -14,5 +16,19 @@ public class SplitArray {
         System.out.println("Nome 2: " + valoresArray[3]);
         System.out.println("Nota 3: " + valoresArray[4]);
         System.out.println("Nome 4: " + valoresArray[5]);
+
+        //List<String> valoresList = new ArrayList<>(List.of(valoresArray));
+        List<String> valoresList = Arrays.asList(valoresArray);
+
+        for (String valorString : valoresList) {
+            System.out.println(valorString);
+        }
+
+        String[] conversaoArray = valoresList.toArray(new String[6]);
+        System.out.println(Arrays.toString(conversaoArray));
+
+        for (String valorString : conversaoArray) {
+            System.out.println(valorString);
+        }
     }
 }
