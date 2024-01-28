0c566f056653aec1dff28016ff4e3fa3fa264c6e
Luiz_E <luizedumartins21@gmail.com>
2024-01-28 15:35:48 -0300

    Módulo 13: Arrays[Vetores] e Matrizes - Aula 13: Lendo uma Matriz multidimensional

--- src/main/java/org/example/executavel/Matriz.java
+++ src/main/java/org/example/executavel/Matriz.java
@@ -4,13 +4,36 @@ import java.util.Arrays;
 
 public class Matriz {
     public static void main(String[] args) {
-        double[][] notas = new double[2][3];
-        notas[0][0] = 80;
-        notas[0][1] = 90;
-        notas[0][2] = 70;
+        double[][] notasTotal = new double[2][3];
+        notasTotal[0][0] = 80;
+        notasTotal[0][1] = 90;
+        notasTotal[0][2] = 70;
 
-        notas[1][0] = 60;
-        notas[1][1] = 85;
-        notas[1][2] = 90;
+        notasTotal[1][0] = 60;
+        notasTotal[1][1] = 85;
+        notasTotal[1][2] = 90;
+
+//        Arrays.stream(notas).forEach(listaNotas -> {
+//            System.out.println("Notas:");
+//            // Método 1
+//         //   Arrays.stream(listaNotas).forEach(System.out::println);
+//            // Método 2
+//            System.out.println(Arrays.toString(listaNotas));
+//        });
+
+//        for (double[] notasDisciplina : notasTotal) {
+//            System.out.println(notasDisciplina);
+//            System.out.println("Disciplina: ");
+//            for (double nota : notasDisciplina) {
+//                System.out.println(nota);
+//            }
+//        }
+
+        for (int linha = 0; linha < notasTotal.length; linha++) {
+            for (int coluna = 0; coluna < notasTotal[linha].length; coluna++) {
+                System.out.printf("Valor na posição [%d][%d]: %.2f%n", linha, coluna, notasTotal[linha][coluna]);
+            }
+            System.out.println("-".repeat(30));
+        }
     }
 }
