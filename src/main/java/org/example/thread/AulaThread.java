6aa3ed621cdbf58846d75fbfa8b42e48aaabf9f7
Luiz_E <luizedumartins21@gmail.com>
2024-01-29 21:03:52 -0300

    Módulo 14: Thread e programação paralela ou concorrente - Aula 3: Criando a primeira Thread

--- src/main/java/org/example/thread/AulaThread.java
+++ src/main/java/org/example/thread/AulaThread.java
@@ -1,13 +1,23 @@
 package org.example.thread;
 
+import javax.swing.*;
 import java.lang.Thread;
 public class AulaThread {
 
-    public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i <  10; i++) {
-            Thread.sleep(1000);
-            System.out.println("Executando rotina.");
-        }
+    public static void main(String[] args) {
+        new Thread(() -> {
+            for (int i = 0; i <  10; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Executando rotina.");
+            }
+        }).start();
+
+        System.out.println("Finalizou o teste.");
+        JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário.");
     }
 
 }
