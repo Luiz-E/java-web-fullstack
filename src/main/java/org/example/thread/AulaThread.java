d99f6be2e190e0ecfa6144f24a09c874368cf051
Luiz_E <luizedumartins21@gmail.com>
2024-01-31 20:32:11 -0300

    Módulo 14: Thread e programação paralela ou concorrente - Aula 5: Thread em objeto e o Runnable

--- src/main/java/org/example/thread/AulaThread.java
+++ src/main/java/org/example/thread/AulaThread.java
@@ -5,30 +5,34 @@ import java.lang.Thread;
 public class AulaThread {
 
     public static void main(String[] args) {
-        new Thread(() -> {
-            for (int i = 0; i <  10; i++) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Executando rotina 1.");
-            }
-        }).start();
+        Thread rotina1 = new Thread(thread1);
+        Thread rotina2 = new Thread(thread2);
 
-        new Thread(() -> {
-            for (int i = 0; i <  10; i++) {
-                try {
-                    Thread.sleep(1300);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Executando rotina 2.");
-            }
-        }).start();
+        rotina1.start();
+        rotina2.start();
 
         System.out.println("Finalizou o teste.");
         JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário.");
     }
 
+    private static Runnable thread1 = () -> {
+        for (int i = 0; i <  10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Executando rotina 1.");
+        }
+    };
+    private static Runnable thread2 = () -> {
+        for (int i = 0; i <  10; i++) {
+            try {
+                Thread.sleep(1300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Executando rotina 2.");
+        }
+    };
 }
