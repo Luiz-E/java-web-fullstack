a9279668ddcc8dcaaf829be09a29c2c400a191ed
Luiz_E <luizedumartins21@gmail.com>
2024-02-01 19:59:54 -0300

    Módulo 14: Thread e programação paralela ou concorrente - Aula 11: Criando a Fila e Pilha para processar paralelo em Thread

---
+++ src/main/java/org/example/thread/ImplementacaoFilaThread.java
@@ -0,0 +1,38 @@
+package org.example.thread;
+
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ImplementacaoFilaThread extends Thread {
+    private static ConcurrentLinkedQueue<FilaThread> pilha_fila = new ConcurrentLinkedQueue<>();
+
+    public static void add(FilaThread filaThread) {
+        pilha_fila.add(filaThread);
+    }
+
+    @Override
+    public void run() {
+        Iterator<FilaThread> iterator = pilha_fila.iterator();
+        synchronized (iterator) {
+            while (iterator.hasNext()) {
+                FilaThread processo = iterator.next();
+                System.out.println("---------------------------------------");
+                System.out.println("Executando processo: " + processo.getNome());
+                System.out.println("Email: " + processo.getEmail());
+                System.out.println("---------------------------------------");
+                iterator.remove();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+}
