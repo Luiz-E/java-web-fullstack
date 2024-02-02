5d228aa40b00c4622c4af2e1d6c5b3c33cb8dbd7
Luiz_E <luizedumartins21@gmail.com>
2024-02-01 21:25:40 -0300

    Módulo 15: Datas, tempo e período com API DateTime - Aula 2: Formatando datas com o SimpleDateFormat

--- src/main/java/org/example/src/DatasJava.java
+++ src/main/java/org/example/src/DatasJava.java
@@ -1,9 +1,11 @@
 package org.example.src;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatasJava {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Date date = new Date();
         
         System.out.println("Milisegundos: " + date.getTime());
@@ -14,5 +16,19 @@ public class DatasJava {
         System.out.println("Segundo: " + date.getSeconds());
         System.out.println("Ano: " + date.getYear());
         System.out.println("Ano atual: " + (date.getYear() + 1900));
+
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        System.out.println(format.format(date));
+        format = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(format.format(date));
+        format = new SimpleDateFormat("yy-MM-dd");
+        System.out.println(format.format(date));
+        format = new SimpleDateFormat("yy-MMMM-dd");
+        System.out.println(format.format(date));
+        format = new SimpleDateFormat("yy-MM-dddd");
+        System.out.println(format.format(date));
+        format = new SimpleDateFormat("yy-MM-dddd HH:mm.s");
+        System.out.println(format.format(date));
+        System.out.println(format.parse("2025-12-13 15:33.23"));
     }
 }
