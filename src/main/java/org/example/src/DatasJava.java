ae6b8c66066c205bdaeea1bb95a7d8f12fa9e81d
Luiz_E <luizedumartins21@gmail.com>
2024-02-04 20:07:47 -0300

    Módulo 15: Datas, tempo e período com API DateTime - Aula 4: After e Before comparando datas menores e maiores

--- src/main/java/org/example/src/DatasJava.java
+++ src/main/java/org/example/src/DatasJava.java
@@ -2,52 +2,23 @@ package org.example.src;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class DatasJava {
     public static void main(String[] args) throws ParseException {
-        Calendar calendar = Calendar.getInstance();
-        Date date = new Date();
-        
-        System.out.println("Milisegundos: " + date.getTime());
-        System.out.println("Calendar milissegundos: " + calendar.getTimeInMillis());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date dataVencimentoBoleto = simpleDateFormat.parse("10/03/2024");
+        Date dataAtual = simpleDateFormat.parse("11/03/2024");
+//        if (dataVencimentoBoleto.after(dataAtual)) {
+//            System.out.println("Não venceu");
+//        } else {
+//            System.out.println("Venceu");
+//        }
 
-        System.out.println("Dia do mes: " + date.getDate());
-        System.out.println("Calendar dia: " + calendar.get(Calendar.DAY_OF_MONTH));
-
-        System.out.println("Dia da semana: " + date.getDay());
-        System.out.println("Calendar semana: " + (calendar.get(Calendar.DAY_OF_WEEK) - 1) );
-
-        System.out.println("Hora do dia: " + date.getHours());
-        System.out.println("Hora: " + calendar.get(Calendar.HOUR_OF_DAY));
-
-        System.out.println("Minuto da hora: " + date.getMinutes());
-        System.out.println("Minuto: " + calendar.get(Calendar.MINUTE));
-
-        System.out.println("Segundo: " + date.getSeconds());
-        System.out.println("Segundos: " + calendar.get(Calendar.SECOND));
-
-        System.out.println("Ano: " + date.getYear());
-        System.out.println("Ano: " + calendar.get(Calendar.YEAR));
-
-        System.out.println("Ano atual: " + (date.getYear() + 1900));
-
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-
-        System.out.println("Data atual date: " + format.format(date));
-        System.out.println("Data atual calendar: " + format.format(calendar.getTime()));
-
-        format = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.println(format.format(date));
-        format = new SimpleDateFormat("yy-MM-dd");
-        System.out.println(format.format(date));
-        format = new SimpleDateFormat("yy-MMMM-dd");
-        System.out.println(format.format(date));
-        format = new SimpleDateFormat("yy-MM-dddd");
-        System.out.println(format.format(date));
-        format = new SimpleDateFormat("yy-MM-dddd HH:mm.s");
-        System.out.println(format.format(date));
-        System.out.println(format.parse("2025-12-13 15:33.23"));
+        if (dataVencimentoBoleto.before(dataAtual)) {
+            System.out.println("Venceu");
+        } else {
+            System.out.println("Não venceu");
+        }
     }
 }
