c31b85975ce3e7aad869724344eed1c1a6ca7fed
Luiz_E <luizedumartins21@gmail.com>
2024-07-23 19:29:03 -0300

    Módulo 15: Datas, tempo e período com API DateTime - Aula 10: Dia, meses, anos e semanas

--- src/main/java/org/example/src/DatasJava.java
+++ src/main/java/org/example/src/DatasJava.java
@@ -1,22 +1,22 @@
 package org.example.src;
 
-import javax.swing.text.DateFormatter;
-import java.text.ParseException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DatasJava {
     public static void main(String[] args) {
 
-        LocalDate dataAtual = LocalDate.now();
-        System.out.println("Data atual: " + dataAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 
-        LocalTime horaAtual = LocalTime.now();
-        System.out.println("Hora atual: " + horaAtual.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        System.out.println("Dia da semana: " + localDate.getDayOfWeek());
+        System.out.println("Dia da semana: " + localDate.getDayOfWeek().ordinal());
+        System.out.println("Dia da semana: " + localDate.getDayOfWeek().name());
 
-        LocalDateTime dataHoraAtual = LocalDateTime.now();
-        System.out.println("Data e hora: "  + dataHoraAtual.format(DateTimeFormatter.ISO_DATE_TIME));
+        System.out.println("Dia do mẽs: " + localDate.getDayOfMonth());
+        System.out.println("Dia do ano: " + localDate.getDayOfYear());
+        System.out.println("Mês: " + localDate.getMonth());
+        System.out.println("Mês: " + localDate.getMonthValue());
+        System.out.println("Ano: " + localDate.getYear());
     }
 }
