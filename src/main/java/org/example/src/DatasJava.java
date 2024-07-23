f7a8b8bb8b3882f9fea9563a4c9d6d18fc8117bb
Luiz_E <luizedumartins21@gmail.com>
2024-07-23 20:05:24 -0300

    Módulo 15: Datas, tempo e período com API DateTime - Aula 12: Faixa de tempo com Objeto Period

--- src/main/java/org/example/src/DatasJava.java
+++ src/main/java/org/example/src/DatasJava.java
@@ -1,22 +1,26 @@
 package org.example.src;
 
-import java.time.Duration;
-import java.time.Instant;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 public class DatasJava {
-    public static void main(String[] args) throws InterruptedException {
-        Instant inicio = Instant.now();
+    public static void main(String[] args) {
 
-        Thread.sleep(2000);
-        Instant iFinal = Instant.now();
+        LocalDate dataAntiga = LocalDate.of(2011, 2, 3);
+        LocalDate dataNova = LocalDate.of(2022, 5, 12);
 
-        Duration duracao = Duration.between(inicio, iFinal);
-        System.out.println("Duração: " + duracao);
-        System.out.println("Duração em nanossegundos: " + duracao.toNanos());
-        System.out.println("Duração em minutos: " + duracao.toMinutes());
-        System.out.println("Duração em horas: " + duracao.toHours());
-        System.out.println("Duração em dias: " + duracao.toDays());
-        System.out.println("Duração em segundos: " + duracao.toSeconds());
-        System.out.println("Duração em milisegundos: " + duracao.toMillis());
+        System.out.println(dataAntiga.isAfter(dataNova));
+        System.out.println(dataAntiga.isBefore(dataNova));
+        System.out.println(dataAntiga.isEqual(dataNova));
+
+        System.out.println("---------------------------------------------");
+        Period period = Period.between(dataAntiga, dataNova);
+
+        System.out.println(period.getDays());
+        System.out.println(period.getMonths());
+        System.out.println(period.getYears());
+        System.out.println("Diferença de " + period.getYears() + " anos, " + period.getMonths() +  " meses e " +  period.getDays() + " dias");
+        System.out.println(period.toTotalMonths());
     }
 }
