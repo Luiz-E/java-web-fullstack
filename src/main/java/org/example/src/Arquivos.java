b7daf19c9ee1655ff714cbf2e0f76a1adb95925f
Luiz_E <luizedumartins21@gmail.com>
2024-07-24 18:56:19 -0300

    Módulo 16: Leitura e escrita TXT e excel com Apache POI e JSON com GSON - Aula 2: Lendo as linhas do arquivo de TXT

--- src/main/java/org/example/src/Arquivos.java
+++ src/main/java/org/example/src/Arquivos.java
@@ -1,41 +1,23 @@
 package org.example.src;
 
+
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Arquivos {
-    public static void main(String[] args) throws IOException {
-
-        Pessoa pessoa1 = new Pessoa();
-        pessoa1.setEmail("pessoa1@gmail.com");
-        pessoa1.setIdade(50);
-        pessoa1.setNome("Alex egidio");
-        Pessoa pessoa2 = new Pessoa();
-        pessoa2.setEmail("pessoa2@gmail.com");
-        pessoa2.setIdade(25);
-        pessoa2.setNome("Marcos Tadeu");
-        Pessoa pessoa3 = new Pessoa();
-        pessoa3.setEmail("pessoa3@gmail.com");
-        pessoa3.setIdade(40);
-        pessoa3.setNome("Maria Jesus");
-
-        List<Pessoa> pessoas = new ArrayList<>(List.of(pessoa1, pessoa2, pessoa3));
-
-        File arquivo = new File("./src/main/java/org/example/src/arquivo.txt");
-        if (!arquivo.exists()) {
-            arquivo.createNewFile();
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream input =
+                new FileInputStream("./src/main/java/org/example/src/arquivo.txt");
+        Scanner lerArquivo = new Scanner(input, StandardCharsets.UTF_8);
+        while(lerArquivo.hasNext()) {
+            String linha = lerArquivo.nextLine();
+            if (linha != null && !linha.isEmpty()) {
+                System.out.println(linha);
+            }
         }
-
-        FileWriter writer = new FileWriter(arquivo);
-
-        for (Pessoa p : pessoas) {
-            writer.write(p.getNome() + ";" + p.getEmail() + ";" + p.getIdade() + "\n");
-        }
-        writer.flush();
-        writer.close();
-
+        lerArquivo.close();
     }
 }
