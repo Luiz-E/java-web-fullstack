90524f128748575e93abd028a2d412c2b4b46660
Luiz_E <luizedumartins21@gmail.com>
2024-07-25 18:17:13 -0300

    Módulo 16: Leitura e escrita TXT e excel com Apache POI e JSON com GSON - Aula 4: Escrevendo e lendo CSV planilha Excel

--- src/main/java/org/example/src/Arquivos.java
+++ src/main/java/org/example/src/Arquivos.java
@@ -1,37 +1,52 @@
 package org.example.src;
 
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Arquivos {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.setEmail("pessoa1@gmail.com");
+        pessoa1.setIdade(50);
+        pessoa1.setNome("Alex egidio");
+        Pessoa pessoa2 = new Pessoa();
+        pessoa2.setEmail("pessoa2@gmail.com");
+        pessoa2.setIdade(25);
+        pessoa2.setNome("Marcos Tadeu");
+        Pessoa pessoa3 = new Pessoa();
+        pessoa3.setEmail("pessoa3@gmail.com");
+        pessoa3.setIdade(40);
+        pessoa3.setNome("Maria Jesus");
+
+        List<Pessoa> pessoas = new ArrayList<>(List.of(pessoa1, pessoa2, pessoa3));
+
+        File arquivo = new File("./src/main/java/org/example/src/arquivo.csv");
+        if (!arquivo.exists()) {
+            arquivo.createNewFile();
+        }
+
+        FileWriter writer = new FileWriter(arquivo);
+
+        for (Pessoa p : pessoas) {
+            writer.write(p.getNome() + ";" + p.getEmail() + ";" + p.getIdade() + "\n");
+        }
+
+
         FileInputStream input =
                 new FileInputStream("./src/main/java/org/example/src/arquivo.txt");
         Scanner lerArquivo = new Scanner(input, StandardCharsets.UTF_8);
-
-        List<Pessoa> pessoas = new ArrayList<>();
-
         while(lerArquivo.hasNext()) {
             String linha = lerArquivo.nextLine();
             if (linha != null && !linha.isEmpty()) {
-                String[] dados = linha.split(";");
-                Pessoa pessoa = new Pessoa();
-                pessoa.setNome(dados[0]);
-                pessoa.setEmail(dados[1]);
-                pessoa.setIdade(Integer.parseInt(dados[2]));
-                pessoas.add(pessoa);
+                System.out.println(linha);
             }
         }
-
-        for (Pessoa pessoa : pessoas) {
-            System.out.println(pessoa);
-        }
         lerArquivo.close();
+
+        writer.flush();
+        writer.close();
     }
 }
