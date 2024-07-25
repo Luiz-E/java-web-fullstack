73bd1b9937a59f2f85cde981e6e3abae05f53793
Luiz_E <luizedumartins21@gmail.com>
2024-07-25 18:40:24 -0300

    Módulo 16: Leitura e escrita TXT e excel com Apache POI e JSON com GSON - Aula 6: lendo Excel XLS com Apache Poi e montando lista

--- src/main/java/org/example/src/Arquivos.java
+++ src/main/java/org/example/src/Arquivos.java
@@ -5,55 +5,41 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Arquivos {
     public static void main(String[] args) throws IOException {
-        File file = new File("./src/main/java/org/example/src/arquivo_excel.xls");
-        if (!file.exists()) {
-            file.createNewFile();
+        FileInputStream file = new FileInputStream("./src/main/java/org/example/src/arquivo_excel.xls");
+        HSSFWorkbook hssfWorkbook = new HSSFWorkbook(file);
+        HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(0);
+
+        Iterator<Row> iterator = hssfSheet.iterator();
+        List<Pessoa> pessoas = new ArrayList<>();
+        while (iterator.hasNext()) {
+            Pessoa pessoa = new Pessoa();
+            Row linha = iterator.next();
+            Iterator<Cell> cellIterator = linha.iterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+
+                switch (cell.getColumnIndex()) {
+                    case 0:
+                        pessoa.setNome(cell.getStringCellValue());
+                        break;
+                    case 1:
+                        pessoa.setEmail(cell.getStringCellValue());
+                        break;
+                    case 2:
+                        pessoa.setIdade(Double.valueOf(cell.getNumericCellValue()).intValue());
+                }
+            }
+            pessoas.add(pessoa);
         }
-
-        Pessoa pessoa1 = new Pessoa();
-        pessoa1.setEmail("pessoa1@gmail.com");
-        pessoa1.setIdade(50);
-        pessoa1.setNome("Alex egidio");
-        Pessoa pessoa2 = new Pessoa();
-        pessoa2.setEmail("pessoa2@gmail.com");
-        pessoa2.setIdade(25);
-        pessoa2.setNome("Marcos Tadeu");
-        Pessoa pessoa3 = new Pessoa();
-        pessoa3.setEmail("pessoa3@gmail.com");
-        pessoa3.setIdade(40);
-        pessoa3.setNome("Maria Jesus");
-
-        List<Pessoa> pessoas = new ArrayList<>(List.of(pessoa1, pessoa2, pessoa3));
-
-        HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
-        HSSFSheet linhasPessoas = hssfWorkbook.createSheet("Planilha de pessoas JDev Treinamento");
-
-        int numeroLinha = 0;
-        for (Pessoa pessoa : pessoas) {
-            Row row = linhasPessoas.createRow(numeroLinha++);
-
-            int numeroCell = 0;
-
-            Cell cellNome = row.createCell(numeroCell++);
-            cellNome.setCellValue(pessoa.getNome());
-
-            Cell cellEmail = row.createCell(numeroCell++);
-            cellEmail.setCellValue(pessoa.getEmail());
-
-            Cell cellIdade = row.createCell(numeroCell);
-            cellIdade.setCellValue(pessoa.getIdade());
-        }
-
-        FileOutputStream saida = new FileOutputStream(file);
-        hssfWorkbook.write(saida);
-        saida.flush();
-        saida.close();
-        System.out.println("Planilha criada. ");
+        file.close();
+        System.out.println(pessoas);
     }
 }
