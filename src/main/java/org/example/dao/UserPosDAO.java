44fb82ee711426957565b17f84ca5dd5176be27e
Luiz_E <luizedumartins21@gmail.com>
2024-10-08 19:15:57 -0300

    Modulo 21: Introdução prática ao SQL e ao JDBC em Java - Aula 10: Criando nosso INSERT ao banco de dados

--- src/main/java/org/example/dao/UserPosDAO.java
+++ src/main/java/org/example/dao/UserPosDAO.java
@@ -1,8 +1,11 @@
 package org.example.dao;
 
 import org.example.conexaojdbc.SingleConnection;
+import org.example.model.UserposJava;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class UserPosDAO {
 
@@ -12,4 +15,21 @@ public class UserPosDAO {
         conn = SingleConnection.getConn();
     }
 
+    public void salvar(UserposJava user) {
+        try {
+            String sql = "insert into userposjava (id, nome, email) values (?, ?, ?)";
+            PreparedStatement insertStatement = conn.prepareStatement(sql);
+            insertStatement.setLong(1, user.getId());
+            insertStatement.setString(2, user.getNome());
+            insertStatement.setString(3, user.getEmail());
+            insertStatement.execute();
+            conn.commit();
+        } catch (Exception e) {
+            try {
+                conn.rollback();
+            } catch (SQLException e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
 }
--- src/test/java/TesteBancoJdbc.java
+++ src/test/java/TesteBancoJdbc.java
@@ -1,4 +1,6 @@
 import org.example.conexaojdbc.SingleConnection;
+import org.example.dao.UserPosDAO;
+import org.example.model.UserposJava;
 import org.junit.Test;
 
 public class TesteBancoJdbc {
@@ -6,6 +8,16 @@ public class TesteBancoJdbc {
     @Test
     public void initBanco() {
         SingleConnection.getConn();
+    }
+
+    @Test
+    public void testInsert() {
+        UserPosDAO userPosDao = new UserPosDAO();
+        UserposJava user = new UserposJava();
+        user.setId(2L);
+        user.setNome("Teste");
+        user.setEmail("teste@teste.com");
+        userPosDao.salvar(user);
     }
 
 }
